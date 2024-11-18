dbaa3aaf27934b60d6fbeae1256e8740d67cf6d2
Bard1en <greger.grym@gmail.com>
2024-11-18 10:37:19 +0100

    hamburger menu

---
+++ hamburger.js
@@ -0,0 +1,7 @@
+const Hamburger = document.querySelector(".hamburger_menu");
+const Nav_Links = document.querySelector(".nav_items_container");
+
+Hamburger.addEventListener("click", () => {
+    Nav_Links.classList.toggle("active");
+    Hamburger.classList.toggle("active");
+});
