36f0b8be277fee9f721ca3c2bf55441c3c070a11
Adel0726 <adel07001@utb.vaxjo.se>
2024-12-15 15:01:00 +0100

    media

--- hamburger.js
+++ hamburger.js
@@ -5,3 +5,14 @@ Hamburger.addEventListener("click", () => {
     Nav_Links.classList.toggle("active");
     Hamburger.classList.toggle("active");
 });
+
+const nav = document.querySelector("nav");
+const navItemsContainer = document.querySelector(".nav_items_container");
+
+function updateNavHeight() {
+    const navHeight = nav.offsetHeight; 
+    navItemsContainer.style.setProperty("--dynamic-top", `${navHeight}px`);
+}
+
+window.addEventListener("load", updateNavHeight);
+window.addEventListener("resize", updateNavHeight);
